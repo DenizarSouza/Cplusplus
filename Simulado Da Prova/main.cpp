e5e57227bded5a3be910c8d82592bbda90837e67
Mateus Moreira <mateusmoreira1805@hotmail.com>
2015-09-22 23:05:03 -0300

    Penultma aula
    Simulado da prova aplicado na penultima aula.

---
+++ Simulado Da Prova/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+typedef vector < int > vetor;
+
+vetor EntradaDados()
+{
+    vetor v;
+    int nro, n;
+
+    cout << "Entre com um valor: ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout<<"Entre com um numero: ";
+        cin >> nro;
+        v.push_back(nro);
+    }
+    return v;
+}
+
+void Grava(const vetor &v)
+{
+    int soma = 0;
+    ofstream arq_saida;
+    arq_saida.open("numeros.txt", ios::out);
+
+    for(int i = 0; i < v.size(); i++)
+    {
+         cout << v[i] << endl;
+         arq_saida  << v[i] << endl;
+         soma += v[i];
+    }
+    cout << "A soma eh: " << soma << endl;
+    arq_saida << "A soma eh: " << soma << endl;
+    arq_saida.close();
+}
+
+int main()
+{
+    vetor v = EntradaDados();
+    Grava(v);
+    return 0;
+}
